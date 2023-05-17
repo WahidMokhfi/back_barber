aa11da07d45e9e3b7e36c005dd76772bc50ef897
Wahid Mokhfi <wahid.mokhfi@lapiscine.pro>
2023-05-17 14:12:58 +0200

    maj/17/05/2023

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const sequelize = require('./db/sequelize');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = 3005;
 
 app.use(cors({
-  origin: 'http://localhost:3001'
+  origin: 'http://localhost:3004'
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3004');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
