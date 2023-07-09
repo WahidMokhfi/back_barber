a547c7481530f4e893132c5914f6eb2afc2c4038
Wahid Mokhfi <wahid.mokhfi@lapiscine.pro>
2023-07-10 01:32:35 +0200

    10/07/2023

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,61 +1,87 @@
 const { Op, UniqueConstraintError, ValidationError } = require('sequelize');
-const { ReviewModel, UserModel } = require('../db/sequelize');
+const { Review, User } = require('../db/sequelize');
 
 exports.findAllReviews = (req, res) => {
-    ReviewModel.findAll({
-        include: [UserModel.scope('withoutPassword')]
+  Review.findAll({
+    include: [
+      {
+        model: User,
+        attributes: { exclude: ['password'] }
+      }
+    ]
+  })
+    .then(results => {
+      const message = "La liste des avis a bien été récupérée";
+      res.json({ message, data: results });
     })
-        .then(results => {
-            const message = "La liste des avis a bien été récupérée";
-            res.json({ message, data: results });
-        })
-        .catch(error => {
-            const message = "La liste des avis n'a pas pu être récupérée";
-            res.status(500).json({ message, data: error });
-        });
+    .catch(error => {
+      const message = "La liste des avis n'a pas pu être récupérée";
+      res.status(500).json({ message, data: error });
+    });
 };
-
-exports.createReview = (req, res) => {
-    ReviewModel.create({
-      comment: req.body.comment,
-      note: req.body.note,
-      user_id: req.userId // Utilisation de la clé étrangère correcte "user_id"
-    })
-      .then(result => {
-        const message = "L'avis a bien été créé";
-        res.json({ message, data: result });
-      })
-      .catch(error => {
-        if (error instanceof UniqueConstraintError || error instanceof ValidationError) {
-          return res.status(400).json({ message: error.message, data: error });
-        }
-        const message = "L'avis n'a pas pu être créé";
-        res.status(500).json({ message, data: error });
-      });
-  };
-  
 
 exports.updateReview = (req, res) => {
-    ReviewModel.update(req.body, {
-        where: {
-            id: req.params.id,
-            user_id: req.user_id
-        }
+  Review.update(req.body, {
+    where: {
+      id: req.params.id,
+      user_id: req.userId
+    }
+  })
+    .then(([rowsUpdated]) => {
+      if (rowsUpdated === 0) {
+        const message = "Le commentaire demandé n'existe pas ou n'appartient pas à l'utilisateur connecté";
+        res.status(404).json({ message });
+      } else {
+        const message = "Le commentaire a bien été modifié";
+        res.json({ message });
+      }
     })
-        .then(([rowsUpdated]) => {
-            if (rowsUpdated === 0) {
-                const message = "Le commentaire demandé n'existe pas ou n'appartient pas à l'utilisateur connecté";
-                res.status(404).json({ message });
-            } else {
-                const message = "Le commentaire a bien été modifié";
-                res.json({ message });
-            }
-        })
-        .catch(error => {
-            if (error instanceof UniqueConstraintError || error instanceof ValidationError) {
-                return res.status(400).json({ message: error.message, data: error });
-            }
-            const message = "Impossible de mettre à jour le commentaire";
-            res.status(500).json({ message });
-        });
+    .catch(error => {
+      if (error instanceof UniqueConstraintError || error instanceof ValidationError) {
+        return res.status(400).json({ message: error.message, data: error });
+      }
+      const message = "Impossible de mettre à jour le commentaire";
+      res.status(500).json({ message });
+    });
 };
+
+exports.deleteReview = (req, res) => {
+  Review.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(() => {
+      const message = "Le commentaire a bien été supprimé";
+      res.json({ message });
+    })
+    .catch(error => {
+      const message = "Impossible de supprimer le commentaire";
+      res.status(500).json({ message });
+    });
+};
+
+exports.createReviewForService = (req, res) => {
+  Review.create({
+    content: req.body.content,
+    user_id: req.userId,
+    service_id: req.params.service_id,
+    rating: req.body.rating,
+    username: req.body.username
+  })
+    .then(result => {
+      const message = "L'avis a bien été créé pour le service spécifié";
+      res.json({ message, data: result });
+    })
+    .catch(error => {
+      if (error instanceof UniqueConstraintError || error instanceof ValidationError) {
+        return res.status(400).json({ message: error.message, data: error });
+      }
+      const message = "L'avis n'a pas pu être créé pour le service spécifié";
+      res.status(500).json({ message, data: error });
+    });
+};
+
+
+
+
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const { UniqueConstraintError, ValidationError } = require('sequelize');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
 router
   .route('/')
-  .get(authController.protect, reviewController.findAllReviews)
-  .post(authController.protect, reviewController.createReview);
+  .get(reviewController.findAllReviews);
 
 router
   .route('/:id')
-  .put(authController.protect, authController.restrictToOwnUser, reviewController.updateReview);
+  .put(authController.protect, authController.restrictTo('admin'), reviewController.updateReview)
+  .delete(authController.protect, authController.restrictTo('admin'), reviewController.deleteReview);
+
+router
+  .route('/services/:service_id/reviews')
+  .post(authController.protect, reviewController.createReviewForService);
 
 module.exports = router;
 
 
+
+  
+  
+  // priorité :
+
+  // modifier / supprimer un review si admin    = ok
+
+  // pouvoir créer une review pour un service   = ok
+
+  //sur le back : sortir les boutons de modif / suppression de services / avis etc
+  // et les afficher dans la liste des services / liste avis etc
+
+
+
+  // ensuite : 
+
+  // lister les reviews d'un service avec un fetch
+
+  // lister les services avec un appel fetch en front
+
+  // globalement : 
+  // sortir toutes les infos en durs et les mettre avec un appel fetch, donc créer tables si besoin
+
+  // créer pages statiques pour cgv etc (avec contenu en dur type lorem ipsum)
+
+  // rechecker responsive tablette
+
+    // envoyer mail au contact form // ou alors afficher le mail, horaires etc
+  // ajouter carte map eventuellement
 
 
 
